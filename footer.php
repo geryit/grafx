95760ca3abd129636914b563e35304679e53b2ca
Goksel Eryigit <geryit@gmail.com>
2016-11-21 02:40:45 +0000

    homepage final

--- footer.php
+++ footer.php
@@ -9,6 +9,18 @@
 
 </div>
 
+<footer class="footer">
+    <div class="container">
+        <div class="footer__wrapper">
+            <div class="footer__left">All rights reserved. Copyright &copy; 2016</div>
+            <div class="footer__right">
+                <a href="#" class="footer__right__link">Legal Disclaimer</a>
+                <a href="#" class="footer__right__link">grafx.co</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 
 
