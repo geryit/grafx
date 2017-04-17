56192c48654847fb011a262861090063fa07897f
goksel eryigit <geryit@gmail.com>
2017-04-18 01:02:22 +0300

    password protected

--- single-work.php
+++ single-work.php
@@ -1,54 +1,59 @@
 <?php
-get_header();
-if (get_query_var('orderby')) $orderby = get_query_var('orderby'); else $orderby = 'date';
+
+if (!post_password_required($post)) {
+
+    get_header();
 
 
-$post_id = $post->ID; // 275
-$post_tax = get_post_taxonomies($post)[1]; // "work-category"
-$post_first_term = wp_get_post_terms($post->ID, $post_tax)[0]->slug; // "brand-design"
-
-$term = get_query_var('term', ''); // get "term" from url = ?term=brand-design = 'brand-design' (if empty, default is first term)
-$s = get_query_var('se', ''); // get "se" from url = ?se=hea = 'hea' (if empty, default is '')
+    if (get_query_var('orderby')) $orderby = get_query_var('orderby'); else $orderby = 'date';
 
 
-$args = array(
-  'post_type' => get_post_type(), // 'work'
-  's' => $s,
-  'orderby' => $orderby,
-  'order' => $order,
+    $post_id = $post->ID; // 275
+    $post_tax = get_post_taxonomies($post)[1]; // "work-category"
+    $post_first_term = wp_get_post_terms($post->ID, $post_tax)[0]->slug; // "brand-design"
 
-);
-
-
-if ($term)
-  $args['tax_query'] = array(
-    array(
-      'taxonomy' => $post_tax,
-      'field' => 'slug',
-      'terms' => $term,
-    )
-  );
+    $term = get_query_var('term', ''); // get "term" from url = ?term=brand-design = 'brand-design' (if empty, default is first term)
+    $s = get_query_var('se', ''); // get "se" from url = ?se=hea = 'hea' (if empty, default is '')
 
 
-$query = new WP_Query($args);
-if ($s) relevanssi_do_query($query); // this works only there is search ('s' => $s,)
+    $args = array(
+        'post_type' => get_post_type(), // 'work'
+        's' => $s,
+        'orderby' => $orderby,
+        'order' => $order,
 
-if ($query->have_posts()) {
-  while ($query->have_posts()) {
-    $query->the_post();
-    $ids[] = $post->ID; // [294,275,274,273,270]
-  }
-  /* Restore original Post Data */
-  wp_reset_postdata();
-}
+    );
 
 
-$thisindex = array_search($post_id, $ids); // 1
+    if ($term)
+        $args['tax_query'] = array(
+            array(
+                'taxonomy' => $post_tax,
+                'field' => 'slug',
+                'terms' => $term,
+            )
+        );
 
 
-$workCount = count($ids); // 5
-if ($thisindex > 0) $previd = $ids[$thisindex - 1]; // $previd = 294
-if ($thisindex < $workCount - 1) $nextid = $ids[$thisindex + 1]; // $nextid = 274
+    $query = new WP_Query($args);
+    if ($s) relevanssi_do_query($query); // this works only there is search ('s' => $s,)
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $ids[] = $post->ID; // [294,275,274,273,270]
+        }
+        /* Restore original Post Data */
+        wp_reset_postdata();
+    }
+
+
+    $thisindex = array_search($post_id, $ids); // 1
+
+
+    $workCount = count($ids); // 5
+    if ($thisindex > 0) $previd = $ids[$thisindex - 1]; // $previd = 294
+    if ($thisindex < $workCount - 1) $nextid = $ids[$thisindex + 1]; // $nextid = 274
 
 //printr($args);
 //printr($ids);
@@ -60,95 +65,99 @@ if ($thisindex < $workCount - 1) $nextid = $ids[$thisindex + 1]; // $nextid = 27
 //if (isset($previd)) printr("previd:" . $previd); else echo "no previd";
 //if (isset($nextid)) printr("nextid:" . $nextid); else echo "no nextid";
 
-?>
+    ?>
 
-<div id="wrap">
-  <div class="workHead"
-       <? if (isset($nextid)) { ?>ng-swipe-left="go('<?= add_query_arg(array('term' => $term, 'se' => $s, 'orderby' => $orderby, 'order' => $order), get_permalink($nextid)) ?>')"<? } ?>
-       <? if (isset($previd)) { ?>ng-swipe-right="go('<?= add_query_arg(array('term' => $term, 'se' => $s, 'orderby' => $orderby, 'order' => $order), get_permalink($previd)) ?>')"<? } ?>
-  >
-    <div class="container">
-      <ul class="workHead__items">
+    <div id="wrap">
+        <div class="workHead"
+             <? if (isset($nextid)) { ?>ng-swipe-left="go('<?= add_query_arg(array('term' => $term, 'se' => $s, 'orderby' => $orderby, 'order' => $order), get_permalink($nextid)) ?>')"<? } ?>
+             <? if (isset($previd)) { ?>ng-swipe-right="go('<?= add_query_arg(array('term' => $term, 'se' => $s, 'orderby' => $orderby, 'order' => $order), get_permalink($previd)) ?>')"<? } ?>
+        >
+            <div class="container">
+                <ul class="workHead__items">
 
 
-        <li class="workHead__nav workHead__nav--prev">
-          <? if (isset($previd)) { ?>
-            <a class="workHead__icon workHead__icon--prev icon-l-arrow2"
-               href="<?= add_query_arg(array('term' => $term, 'se' => $s, 'orderby' => $orderby, 'order' => $order), get_permalink($previd)) ?>"></a>
-          <? } ?>
-        </li>
+                    <li class="workHead__nav workHead__nav--prev">
+                        <? if (isset($previd)) { ?>
+                            <a class="workHead__icon workHead__icon--prev icon-l-arrow2"
+                               href="<?= add_query_arg(array('term' => $term, 'se' => $s, 'orderby' => $orderby, 'order' => $order), get_permalink($previd)) ?>"></a>
+                        <? } ?>
+                    </li>
 
-        <li class="workHead__item">
-          <span class="workHead__head"><?= get_field('brand_title', 'option'); ?></span>
-          <span class="workHead__link light active">
+                    <li class="workHead__item">
+                        <span class="workHead__head"><?= get_field('brand_title', 'option'); ?></span>
+                        <span class="workHead__link light active">
                     <?= boldify(get_the_title()) ?>
                     </span>
-        </li>
-        <li class="workHead__nav workHead__nav--next">
-          <? if (isset($nextid)) { ?>
-            <a class="workHead__icon workHead__icon--next icon-r-arrow2"
-               href="<?= add_query_arg(array('term' => $term, 'se' => $s, 'orderby' => $orderby, 'order' => $order), get_permalink($nextid)) ?>"></a>
-          <? } ?>
-        </li>
-      </ul>
-    </div>
-  </div>
+                    </li>
+                    <li class="workHead__nav workHead__nav--next">
+                        <? if (isset($nextid)) { ?>
+                            <a class="workHead__icon workHead__icon--next icon-r-arrow2"
+                               href="<?= add_query_arg(array('term' => $term, 'se' => $s, 'orderby' => $orderby, 'order' => $order), get_permalink($nextid)) ?>"></a>
+                        <? } ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
-  <div class="workC">
-    <?
+        <div class="workC">
+            <?
 
 
-    if (have_rows('content')) {
+            if (have_rows('content')) {
 
-      // loop through the rows of data
-      while (have_rows('content')) {
-        the_row();
+                // loop through the rows of data
+                while (have_rows('content')) {
+                    the_row();
 //        echo '<div>'.get_row_layout().'</div>';
 
-        if (get_row_layout() == 'main_video') {
+                    if (get_row_layout() == 'main_video') {
 
 
-          if (have_rows('item')) {
-            while (have_rows('item')) {
-              the_row();
-              get_template_part('content', 'main_video');
+                        if (have_rows('item')) {
+                            while (have_rows('item')) {
+                                the_row();
+                                get_template_part('content', 'main_video');
+                            }
+                        }
+                    } elseif (get_row_layout() == 'full_width_text') {
+                        if (have_rows('item')) {
+                            while (have_rows('item')) {
+                                the_row();
+                                get_template_part('content', 'full_width_text');
+                            }
+                        }
+                    } elseif (get_row_layout() == 'image_video_set') {
+                        get_template_part('content', 'image_video_set');
+
+                    } elseif (get_row_layout() == 'half_width_text') {
+                        get_template_part('content', 'half_width_text');
+
+                    } elseif (get_row_layout() == 'slider') {
+                        get_template_part('content', 'slider');
+
+                    } elseif (get_row_layout() == 'credit') {
+                        get_template_part('content', 'credit');
+
+                    } elseif (get_row_layout() == 'image_for_m') {
+                        get_template_part('content', 'image_for_mobile');
+
+                    }
+
+                }
+
             }
-          }
-        } elseif (get_row_layout() == 'full_width_text') {
-          if (have_rows('item')) {
-            while (have_rows('item')) {
-              the_row();
-              get_template_part('content', 'full_width_text');
-            }
-          }
-        } elseif (get_row_layout() == 'image_video_set') {
-          get_template_part('content', 'image_video_set');
+            ?>
+        </div>
 
-        } elseif (get_row_layout() == 'half_width_text') {
-          get_template_part('content', 'half_width_text');
-
-        } elseif (get_row_layout() == 'slider') {
-          get_template_part('content', 'slider');
-
-        } elseif (get_row_layout() == 'credit') {
-          get_template_part('content', 'credit');
-
-        }elseif (get_row_layout() == 'image_for_m') {
-          get_template_part('content', 'image_for_mobile');
-
-        }
-
-      }
-
-    }
-    ?>
-  </div>
-
-  <div class="pluses">
-    <div class="container">
-      <div class="pluses__inner"></div>
+        <div class="pluses">
+            <div class="container">
+                <div class="pluses__inner"></div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 
-<?php get_footer(); ?>
+    <?php get_footer();
+} else {
+    echo "<div style='text-align: center;margin-top: 20px;'>".get_the_password_form()."</div>";
+}
+
--- template-works.php
+++ template-works.php
@@ -8,10 +8,10 @@ $term_id = get_queried_object()->term_id;
 
 //find index in acf list
 foreach ($cats as $i => $v) {
-  if ($v['category']->term_id == $term_id) {
-    $term_index = $i;
-    break;
-  }
+    if ($v['category']->term_id == $term_id) {
+        $term_index = $i;
+        break;
+    }
 }
 
 if (get_query_var('orderby')) $orderby = get_query_var('orderby'); else $orderby = 'date';
@@ -20,132 +20,130 @@ if (get_query_var('order')) $order = get_query_var('order'); else $order = 'DESC
 ?>
 
 
-
-
 <div id="wrap">
-  <div class="headItemsWrap"
-       style="background-image: url(<?= get_field('page_header_image') ?>?<?=VER?>)">
-    <div class="container">
+    <div class="headItemsWrap"
+         style="background-image: url(<?= get_field('page_header_image') ?>?<?= VER ?>)">
+        <div class="container">
 
-      <ul class="headItems headItems--cats">
-        <li class="headItems__item">
+            <ul class="headItems headItems--cats">
+                <li class="headItems__item">
           <span class="headItems__link light active">
             ALL
           </span>
-        </li>
-        <?
-        foreach ($cats as $cat) { ?>
-          <li class="headItems__item">
-            <a href="<?= get_term_link($cat['category']->term_id) ?>"
-               class="headItems__link light <?= $term_id == $cat['category']->term_id ? 'active' : '' ?>">
-              <?= $cat['category']->name ?>
-            </a>
-          </li>
+                </li>
+                <?
+                foreach ($cats as $cat) { ?>
+                    <li class="headItems__item">
+                        <a href="<?= get_term_link($cat['category']->term_id) ?>"
+                           class="headItems__link light <?= $term_id == $cat['category']->term_id ? 'active' : '' ?>">
+                            <?= $cat['category']->name ?>
+                        </a>
+                    </li>
 
-        <? } ?>
+                <? } ?>
 
-      </ul>
-
-    </div>
-
-  </div>
-
-
-  <div class="container">
-    <div class="works works--orderBy-<?= $orderby ?> works--cats">
-      <?
-
-      $year_check = false;
-      $loop = new WP_Query(array('post_type' => 'work', 'posts_per_page' => -1,'orderby' => $orderby, 'order'   => $order,));
-      if ($loop->have_posts()) {
-        while ($loop->have_posts()) {
-          $loop->the_post();
-          $year = get_the_date('Y');
-
-          if ($year !== $year_check) { ?>
-            <div class='works__head'>
-              <div class="works__head__inner container">
-                <div class="works__head__left">
-                  <h5 class='works__head__year'><?= $year ?></h5>
-                </div>
-                <div class="works__head__right">
-                  <h5 class='works__head__sortBy'>Sort by</h5>
-                  <? if ($orderby == 'title') { ?>
-                    <span class='works__head__sortBtn on'>A-Z</span>
-                    <?
-                  } else { ?>
-                    <a
-                      href="<?= esc_url(add_query_arg(array('orderby' => 'title', 'order' => 'ASC'))) ?>"
-                      class='works__head__sortBtn'>A-Z</a>
-                    <?
-                  } ?>
-
-                  <? if ($orderby == 'date') { ?>
-                    <span class='works__head__sortBtn on'>DATE</span>
-                    <?
-                  } else { ?>
-                    <a
-                      href="<?= esc_url(add_query_arg(array('orderby' => 'date', 'order' => 'DESC'))) ?>"
-                      class='works__head__sortBtn'>DATE</a>
-                    <?
-                  } ?>
-
-
-                </div>
-              </div>
-
-            </div>
-          <? }
-
-          // Now that your year has been printed, assign it to the $year_check variable
-          $year_check = $year;
-
-
-          get_template_part('loop', 'work');
-        }
-      }
-
-
-      ?>
-    </div>
-
-
-    <div class="reelsItems">
-      <h5 class="reelsItems__head"><?= get_field('reel_category_page_title', REELPAGEID); ?></h5>
-
-
-      <div
-        class="works__item"
-        ng-click="go('<?=get_permalink(REELPAGEID)?>')">
-        <div class="works__inner"
-             style="background-image: url(<?= get_field('main_image', REELPAGEID); ?>?<?= VER ?>)">
-          <span class="works__inner__cornerTitle"
-                style="color: <?= get_field('corner_title_color', REELPAGEID); ?>"><?= get_field('reel_title', REELPAGEID); ?></span>
-          <div class="works__inner__items">
-            <span class="works__inner__title"><?= get_field('reel_title', REELPAGEID); ?></span>
-            <span
-              class="works__inner__desc light"><?= get_field('short_description', REELPAGEID); ?></span>
-            <a
-              href="<?=get_permalink(REELPAGEID)?>"
-              class="diagonalBtn works__inner__btn">
-              <span><?= get_field('button_label', REELPAGEID); ?></span>
-            </a>
-          </div>
+            </ul>
 
         </div>
 
-      </div>
-
-
     </div>
 
 
-  </div>
-  <div class="pluses">
     <div class="container">
-      <div class="pluses__inner"></div>
+
+        <div class="reelsItems">
+            <h5 class="reelsItems__head"><?= get_field('reel_category_page_title', REELPAGEID); ?></h5>
+
+
+            <div
+                    class="works__item"
+                    ng-click="go('<?= get_permalink(REELPAGEID) ?>')">
+                <div class="works__inner"
+                     style="background-image: url(<?= get_field('main_image', REELPAGEID); ?>?<?= VER ?>)">
+          <span class="works__inner__cornerTitle"
+                style="color: <?= get_field('corner_title_color', REELPAGEID); ?>"><?= get_field('reel_title', REELPAGEID); ?></span>
+                    <div class="works__inner__items">
+                        <span class="works__inner__title"><?= get_field('reel_title', REELPAGEID); ?></span>
+                        <span
+                                class="works__inner__desc light"><?= get_field('short_description', REELPAGEID); ?></span>
+                        <a
+                                href="<?= get_permalink(REELPAGEID) ?>"
+                                class="diagonalBtn works__inner__btn">
+                            <span><?= get_field('button_label', REELPAGEID); ?></span>
+                        </a>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="works works--orderBy-<?= $orderby ?> works--cats">
+            <?
+
+            $year_check = false;
+            $loop = new WP_Query(array('post_type' => 'work', 'posts_per_page' => -1, 'orderby' => $orderby, 'order' => $order,));
+            if ($loop->have_posts()) {
+                while ($loop->have_posts()) {
+                    $loop->the_post();
+                    $year = get_the_date('Y');
+
+                    if ($year !== $year_check) { ?>
+                        <div class='works__head'>
+                            <div class="works__head__inner container">
+                                <div class="works__head__left">
+                                    <h5 class='works__head__year'><?= $year ?></h5>
+                                </div>
+                                <div class="works__head__right">
+                                    <h5 class='works__head__sortBy'>Sort by</h5>
+                                    <? if ($orderby == 'title') { ?>
+                                        <span class='works__head__sortBtn on'>A-Z</span>
+                                        <?
+                                    } else { ?>
+                                        <a
+                                                href="<?= esc_url(add_query_arg(array('orderby' => 'title', 'order' => 'ASC'))) ?>"
+                                                class='works__head__sortBtn'>A-Z</a>
+                                        <?
+                                    } ?>
+
+                                    <? if ($orderby == 'date') { ?>
+                                        <span class='works__head__sortBtn on'>DATE</span>
+                                        <?
+                                    } else { ?>
+                                        <a
+                                                href="<?= esc_url(add_query_arg(array('orderby' => 'date', 'order' => 'DESC'))) ?>"
+                                                class='works__head__sortBtn'>DATE</a>
+                                        <?
+                                    } ?>
+
+
+                                </div>
+                            </div>
+
+                        </div>
+                    <? }
+
+                    // Now that your year has been printed, assign it to the $year_check variable
+                    $year_check = $year;
+
+
+                    get_template_part('loop', 'work');
+                }
+            }
+
+
+            ?>
+        </div>
+
+
     </div>
-  </div>
+    <div class="pluses">
+        <div class="container">
+            <div class="pluses__inner"></div>
+        </div>
+    </div>
 </div>
 
 <? get_footer(); ?>
