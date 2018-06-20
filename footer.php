68863a102e8a08dfa4281cce1ee348ebb8e19cfb
Bulent Kocaman <me@bulentkocaman.com>
2018-06-20 12:13:51 +0300

    All rights reserved text changed.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container">
     <div class="footer__wrapper">
-      <div class="footer__left">All rights reserved. Copyright &copy; 2016</div>
+      <div class="footer__left">ALL RIGHTS RESERVED SINCE © 2013 GRAFX™ - GRAFX IS A REGISTERED TRADEMARK - TM NO: 5450349</div>
       <div class="footer__right">
         <a href="<?= site_url() ?>/disclaimer" class="footer__right__link">Legal Disclaimer</a>
         <a href="#" class="footer__right__link">grafx.co</a>
